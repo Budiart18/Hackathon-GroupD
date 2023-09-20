f9cfb3d4713516ea8db0c823d755699b72cde3a6
ryhanhxx <rayhan328967@gmail.com>
2023-09-20 21:19:08 +0700

    grid layout

--- app/src/main/java/com/aeryz/hackathon3_groupd/presentation/fruitlist/FruitListFragment.kt
+++ app/src/main/java/com/aeryz/hackathon3_groupd/presentation/fruitlist/FruitListFragment.kt
@@ -5,56 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.aeryz.hackathon3_groupd.R
+import com.aeryz.hackathon3_groupd.data.ProductDataSourceImpl
+import com.aeryz.hackathon3_groupd.databinding.FragmentFruitListBinding
+import com.aeryz.hackathon3_groupd.presentation.fruitlist.adapter.FruitListAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FruitListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FruitListFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentFruitListBinding
+    private val adapter: FruitListAdapter by lazy {
+        FruitListAdapter {
+            /*navigateToDetailFragment(it)*/
         }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fruit_list, container, false)
+    ): View {
+        binding = FragmentFruitListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FruitListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FruitListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupGridRecyclerview()
+    }
+
+    private fun setupGridRecyclerview() {
+        binding.rvFruit.adapter = adapter
+        binding.rvFruit.layoutManager = GridLayoutManager(requireContext(), 2 )
+        adapter.setData(ProductDataSourceImpl().getFruits())
     }
 }
--- app/src/main/java/com/aeryz/hackathon3_groupd/presentation/fruitlist/adapter/FruitListAdapter.kt
+++ app/src/main/java/com/aeryz/hackathon3_groupd/presentation/fruitlist/adapter/FruitListAdapter.kt
@@ -1,4 +1,58 @@
 package com.aeryz.hackathon3_groupd.presentation.fruitlist.adapter
 
-class FruitListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.aeryz.hackathon3_groupd.data.ProductDataSource
+import com.aeryz.hackathon3_groupd.databinding.FruitItemListBinding
+import com.aeryz.hackathon3_groupd.model.Product
+
+class FruitListAdapter(private val onItemClick: (Product) -> Unit): RecyclerView.Adapter<FruitListViewHolder>()
+{
+    private val differ = AsyncListDiffer(this, object : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem:  Product): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    })
+
+    fun setData(data: List<Product>) {
+        differ.submitList(data)
+        notifyItemRangeChanged(0, data.size)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitListViewHolder {
+        return FruitListViewHolder(
+            binding = FruitItemListBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            ),
+            onItemClick = onItemClick
+        )
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onBindViewHolder(holder: FruitListViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+}
+
+class FruitListViewHolder(
+    private val binding: FruitItemListBinding,
+    private val onItemClick: (Product) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Product) {
+        binding.root.setOnClickListener {
+            onItemClick.invoke(item)
+        }
+        binding.ivImage.load(item.imgUrl)
+        binding.tvName.text = item.name
+        binding.tvPrice.text = item.price.toString()
+    }
 }
