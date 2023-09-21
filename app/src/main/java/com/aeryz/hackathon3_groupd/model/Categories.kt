a047dcd262ada67eb1055350b6002e96c2f12fec
root <panipujayanti@gmail.com>
2023-09-21 15:36:20 +0700

    finish feature rv category menu

--- app/src/main/java/com/aeryz/hackathon3_groupd/model/Categories.kt
+++ app/src/main/java/com/aeryz/hackathon3_groupd/model/Categories.kt
@@ -1,7 +1,12 @@
 package com.aeryz.hackathon3_groupd.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.UUID
+
+@Parcelize
 data class Categories(
-    val position: Int,
+    val id: String = UUID.randomUUID().toString(),
     val name: String,
     val imgUrl: String
-)
+) : Parcelable
